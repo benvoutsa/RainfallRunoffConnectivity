5ecc4fa24464beee37c46130b6c9ea3f0902db3b
Venetia Voutsa <vvenetia@jacobs-university.de>
2026-05-04 00:06:18 +0000

    update R files

--- ridge_plots_lib.R
+++ ridge_plots_lib.R
@@ -93,7 +93,8 @@ plot_Q_ridges <- function(df_long, cluster_labels, flume_order, ca_fill = TRUE) 
   option = "A",
   trans = scales::pseudo_log_trans(sigma = 0.0001),
   breaks = c(0.005, 0.01, 0.05, 0.1, 1, 10, 100),
-  labels = scales::label_number(accuracy = 0.001, trim = TRUE),
+  labels = scales::label_number_auto(),
+	  #scales::label_number(accuracy = 0.001, trim = TRUE),
   name = expression("contributing area (" * km^2 * ")")
 ) +
 guides(
