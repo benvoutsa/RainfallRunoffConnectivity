0bce3aa8c72df3807b61f79327a0be94deda37a8
Venetia Voutsa <48393906+benvoutsa@users.noreply.github.com>
2026-05-04 23:38:06 +0200

    Update hydrographs_hyetographs.py

--- hydrographs_hyetographs.py
+++ hydrographs_hyetographs.py
@@ -405,7 +405,7 @@ for i, (e1, e2) in enumerate(event_pairs):
 
     cax1 = fig.add_axes([
         bbox1.x0 + 0.03,
-        bbox1.y0 - 0.10,
+        bbox1.y0 - 0.14,
         bbox1.width - 0.06,
         0.025
     ])
@@ -444,7 +444,7 @@ for i, (e1, e2) in enumerate(event_pairs):
 
     cax2 = fig.add_axes([
         bbox2.x0 + 0.03,
-        bbox2.y0 - 0.10,
+        bbox2.y0 - 0.14,
         bbox2.width - 0.06,
         0.025
     ])
