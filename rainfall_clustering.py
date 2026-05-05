2024df1a0581d9bcc1447d19780c73a2104b468a
Venetia Voutsa <48393906+benvoutsa@users.noreply.github.com>
2026-05-05 13:39:42 +0200

    Update rainfall_clustering.py

--- rainfall_clustering.py
+++ rainfall_clustering.py
@@ -35,6 +35,7 @@ df_scaled.head()
 # -------------------------------
 Z = linkage(data_scaled, method='ward', metric="euclidean")
 clusters = fcluster(Z, MAX_D, criterion='distance')
+print(clusters)
 df_scaled['cluster'] = clusters
 print(f"Number of clusters after cut (height {MAX_D}): {len(set(clusters))}")
 
