6083c00ea50bf32e1118eadb5b6262ab4267bef8
Venetia Voutsa <48393906+benvoutsa@users.noreply.github.com>
2026-05-05 14:50:07 +0200

    Update rainfall_clustering.py

--- rainfall_clustering.py
+++ rainfall_clustering.py
@@ -261,7 +261,7 @@ cluster_map = dict(zip(clusters, CUSTOM_COLORS[:n_clusters]))
 
 print(n_clusters)
 
-fig, axes = plt.subplots(1, n_clusters, figsize=(14,2), sharex=True, sharey=True)
+fig, axes = plt.subplots(0, n_clusters-1, figsize=(14,2), sharex=True, sharey=True)
 cluster_colors = CUSTOM_COLORS
 
 if n_clusters == 1:
