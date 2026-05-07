6a587a4d280d08b5d9f14c882777b36dc5553ac6
Venetia Voutsa <48393906+benvoutsa@users.noreply.github.com>
2026-05-08 00:27:18 +0200

    Update hydrographs_hyetographs.py

--- hydrographs_hyetographs.py
+++ hydrographs_hyetographs.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 import datatree as dtree
 import xarray as xr
