d8684fe8d171565de5b4941e3ca077ca04d470e7
Venetia Voutsa <48393906+benvoutsa@users.noreply.github.com>
2026-05-07 11:31:18 +0200

    Update hydrographs_hyetographs.py

--- hydrographs_hyetographs.py
+++ hydrographs_hyetographs.py
@@ -419,7 +419,7 @@ for i, (e1, e2) in enumerate(event_pairs):
 
             fig.text(
                 bbox1.x1,
-                bbox1.y0 - 0.05,
+                bbox1.y0 - 0.08,
                 date1,
                 ha="right",
                 va="top",
@@ -442,7 +442,7 @@ for i, (e1, e2) in enumerate(event_pairs):
 
             fig.text(
                 bbox2.x1,
-                bbox2.y0 - 0.05,
+                bbox2.y0 - 0.07,
                 date2,
                 ha="right",
                 va="top",
