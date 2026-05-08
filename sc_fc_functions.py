8ca93199ae2b16dae7b23f9057e6641942cecbc4
Venetia Voutsa <48393906+benvoutsa@users.noreply.github.com>
2026-05-08 18:01:37 +0200

    Update sc_fc_functions.py

--- sc_fc_functions.py
+++ sc_fc_functions.py
@@ -80,8 +80,8 @@ def compute_sc_sim(df_flume_coordinates, df_contributing_area):
     #df_sc_sim['flume_1'] = df_sc_sim['flume_1'].astype(int).astype(str)
     #df_sc_sim['flume_2'] = df_sc_sim['flume_2'].astype(int).astype(str)
     #print(df_sc_sim)
-    #df_sc_sim['flume_1'] = df_sc_sim['flume_1'].apply(lambda x: f"flume_{x}")
-    #df_sc_sim['flume_2'] = df_sc_sim['flume_2'].apply(lambda x: f"flume_{x}")
+    df_sc_sim['flume_1'] = df_sc_sim['flume_1'].apply(lambda x: f"flume_{x}")
+    df_sc_sim['flume_2'] = df_sc_sim['flume_2'].apply(lambda x: f"flume_{x}")
 
     flume_order_rev = flume_order[::-1]
     print("flume_order_rev: ", flume_order_rev)
@@ -162,8 +162,8 @@ def run_scfc_analysis(runoff_file: Path):
     runoff_tree = load_runoff_events(runoff_file)
 
     # Structural adjacency
-    adj_sim, flume_nums = compute_sc_sim(df_coords, df_areas)
-    flume_labels = [f"flume_{i}" for i in flume_nums]
+    adj_sim, flume_labels = compute_sc_sim(df_coords, df_areas)
+    #flume_labels = [f"flume_{i}" for i in flume_nums]
     print("adj sim")
     print(adj_sim)
     print("---------------------------------------")
