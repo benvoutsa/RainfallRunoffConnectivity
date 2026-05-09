98da3973d53b2e57daff6bf11f032e9cb04caf10
Venetia Voutsa <48393906+benvoutsa@users.noreply.github.com>
2026-05-09 18:21:00 +0200

    Update hydrographs_hyetographs.py

--- hydrographs_hyetographs.py
+++ hydrographs_hyetographs.py
@@ -184,7 +184,7 @@ for i, (e1, e2) in enumerate(event_pairs):
     # ------------------------------------------------------------
     # figure + grid
     # ------------------------------------------------------------
-    fig = plt.figure(figsize=(22, 14))
+    fig = plt.figure(figsize=(22, 12))
     fig.tight_layout(rect=[0, 0.05, 1, 1])
     gs = gridspec.GridSpec(2, 4, figure=fig)
 
@@ -222,7 +222,7 @@ for i, (e1, e2) in enumerate(event_pairs):
         sm.set_array([])
         
         bbox = ax_hydro.get_position()
-        cax = fig.add_axes([bbox.x0 +  0.025, bbox.y0 - 0.12, bbox.width - 0.05, 0.012])
+        cax = fig.add_axes([bbox.x0 +  0.025, bbox.y0 - 0.08, bbox.width - 0.05, 0.012])
         cbar = fig.colorbar(sm, cax=cax, orientation="horizontal", fraction=0.05, pad=0.05)
         
         # Format colorbar ticks to display only the min and max values
@@ -235,7 +235,7 @@ for i, (e1, e2) in enumerate(event_pairs):
         full_date = pd.to_datetime(runoff_ds.time.values[-1]).strftime("%d-%b-%Y")
 
         # write the date under hydrograph
-        fig.text(bbox.x1 + 0.01, bbox.y0 - 0.1, full_date, ha="right", va="top", fontsize=11, color="black", fontweight="bold")
+        fig.text(bbox.x1 + 0.01, bbox.y0 - 0.04, full_date, ha="right", va="top", fontsize=11, color="black", fontweight="bold")
 
         if event_label in rainfall_events:
             rain_max = max([float(data.max().values)
