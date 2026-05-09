4279541821fabaafd5be2bc10e849b91524087de
Venetia Voutsa <48393906+benvoutsa@users.noreply.github.com>
2026-05-09 21:51:00 +0200

    Update hydrographs_hyetographs.py

--- hydrographs_hyetographs.py
+++ hydrographs_hyetographs.py
@@ -207,9 +207,10 @@ for i, (e1, e2) in enumerate(event_pairs):
             ax_rain.tick_params(axis='y', right=False, labelright=False)
             ax_rain.spines['right'].set_visible(False)
         
-        else:
+        elif col_idx == 1:
             ax_hydro = fig.add_subplot(gs[0, 2:4])
             ax_rain = ax_hydro.twinx()
+            ax_rain.set_ylabel("Rainfall (mm/hr)", color="blue")
             # show rainfall only
             ax_hydro.set_ylabel("")
             ax_hydro.yaxis.label.set_visible(False)
@@ -217,7 +218,6 @@ for i, (e1, e2) in enumerate(event_pairs):
             ax_hydro.tick_params(axis='y', left=False, labelleft=False)
             ax_hydro.spines['left'].set_visible(False)
         
-            ax_rain.set_ylabel("Rainfall (mm/hr)", color="blue")
             ax_rain.yaxis.label.set_visible(True)
         
             ax_rain.tick_params(axis='y', colors='blue', labelright=True)
